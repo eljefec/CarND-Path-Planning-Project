9a174422b122e4a3465e5faf6d6bbfdf90177ae2
Jeff <writetojeffrey@gmail.com>
2017-08-26 20:26:38 -0700

    Refactor: Extract JMT into header file.

---
+++ src/poly_main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+#include "poly_solver.h"
+
+using namespace std;
+
+bool close_enough(vector< double > poly, vector<double> target_poly, double eps=0.01) {
+
+
+    if(poly.size() != target_poly.size())
+    {
+        cout << "your solution didn't have the correct number of terms" << endl;
+        return false;
+    }
+    for(int i = 0; i < poly.size(); i++)
+    {
+        double diff = poly[i]-target_poly[i];
+        if(abs(diff) > eps)
+        {
+            cout << "at least one of your terms differed from target by more than " << eps << endl;
+            return false;
+        }
+
+    }
+    return true;
+}
+
+struct test_case {
+
+        vector<double> start;
+        vector<double> end;
+        double T;
+};
+
+vector< vector<double> > answers = {{0.0, 10.0, 0.0, 0.0, 0.0, 0.0},{0.0,10.0,0.0,0.0,-0.625,0.3125},{5.0,10.0,1.0,-3.0,0.64,-0.0432}};
+
+int main() {
+
+    //create test cases
+
+    vector< test_case > tc;
+
+    test_case tc1;
+    tc1.start = {0,10,0};
+    tc1.end = {10,10,0};
+    tc1.T = 1;
+    tc.push_back(tc1);
+
+    test_case tc2;
+    tc2.start = {0,10,0};
+    tc2.end = {20,15,20};
+    tc2.T = 2;
+    tc.push_back(tc2);
+
+    test_case tc3;
+    tc3.start = {5,10,2};
+    tc3.end = {-30,-20,-4};
+    tc3.T = 5;
+    tc.push_back(tc3);
+
+    bool total_correct = true;
+    for(int i = 0; i < tc.size(); i++)
+    {
+        vector< double > jmt = JMT(tc[i].start, tc[i].end, tc[i].T);
+        bool correct = close_enough(jmt,answers[i]);
+        total_correct &= correct;
+
+    }
+    if(!total_correct)
+    {
+        cout << "Try again!" << endl;
+    }
+    else
+    {
+        cout << "Nice work!" << endl;
+    }
+
+    return 0;
+}
---
+++ src/poly_solver.h
@@ -0,0 +1,8 @@
+#ifndef POLY_SOLVER_H
+#define POLY_SOLVER_H
+
+#include <vector>
+
+std::vector<double> JMT(std::vector<double> start, std::vector<double> end, double T);
+
+#endif
