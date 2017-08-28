27429624a94913ba6b81760449790bf175b2e674
Jeff <writetojeffrey@gmail.com>
2017-08-27 17:40:34 -0700

    Refactor: Extract state_diff_cost.

--- src/cost_functions.cpp
+++ src/cost_functions.cpp
@@ -36,6 +36,28 @@ std::vector<Polynomial> get_function_and_derivatives(const VectorXd& coeffs, int
     return polynomials;
 }
 
+double state_diff_cost(double goal_t,
+                       const Vector3d& target_state,
+                       const Vector3d& trajectory_coefficients,
+                       const Vector3d& stddev)
+{
+    auto function_and_derivatives = get_function_and_derivatives(trajectory_coefficients, 2);
+
+    Vector3d trajectory;
+    for (int i = 0; i < 3; i++)
+    {
+        trajectory[i] = function_and_derivatives[i].evaluate(goal_t);
+    }
+
+    double cost = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        double diff = abs(trajectory[i] - target_state[i]);
+        cost += logistic(diff / stddev[i]);
+    }
+    return cost;
+}
+
 double s_diff_cost(const Trajectory& trajectory,
                    int target_vehicle,
                    const VectorXd& delta,
@@ -45,18 +67,5 @@ double s_diff_cost(const Trajectory& trajectory,
     VectorXd target_state = vehicles[target_vehicle].state_in(goal_t) + delta;
     Vector3d s_target = target_state.head(3);
 
-    auto function_and_derivatives = get_function_and_derivatives(trajectory.s_coefficients, 2);
-    Vector3d s_trajectory;
-    for (int i = 0; i < 3; i++)
-    {
-        s_trajectory[i] = function_and_derivatives[i].evaluate(goal_t);
-    }
-
-    double cost = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        double diff = abs(s_trajectory[i] - s_target[i]);
-        cost += logistic(diff / SIGMA_S[i]);
-    }
-    return cost;
+    return state_diff_cost(goal_t, s_target, trajectory.s_coefficients, SIGMA_S);
 }
